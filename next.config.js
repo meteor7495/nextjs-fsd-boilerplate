d7aeecf22def0889f5653fbfb4f2456b5b4dd309
meteor7495 <job.mohiti@gmail.com>
2024-08-24 15:42:34 +0330

    mantine - axios - storybook and some http changes add to prject

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+const nextConfig = {
+    experimental: {
+        optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+    },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
