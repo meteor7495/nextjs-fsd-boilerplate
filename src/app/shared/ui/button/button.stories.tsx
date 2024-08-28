209d04ec2067011b30b0b42a4b28e4b2cf871a69
meteor7495 <job.mohiti@gmail.com>
2024-08-28 14:08:05 +0330

    .

--- src/app/shared/ui/button/button.stories.tsx
+++ src/app/shared/ui/button/button.stories.tsx
@@ -1,16 +1,21 @@
 import { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './button';
+import { MantineProvider } from '@mantine/core';
 
 const meta: Meta<typeof Button> = {
-  component: Button,
-  tags: ['autodocs'],
-  decorators: [
-    (Story) => {
-      document.documentElement.classList.add('dark');
-      return <Story />;
-    },
-  ],
+    component: Button,
+    tags: ['autodocs'],
+    decorators: [
+        (Story) => {
+            document.documentElement.classList.add('dark');
+            return (
+                <MantineProvider>
+                    <Story />
+                </MantineProvider>
+            );
+        },
+    ],
 };
 
 export default meta;
@@ -18,9 +23,13 @@ export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const BrandColors: Story = {
-  render: () => (
-    <>
-      <Button>Default</Button>
-    </>
-  ),
+    render: () => <Button>Default</Button>,
+    parameters: {
+        docs: {
+            source: {
+                // Override the source code to display a simplified version
+                code: '<Button>Default</Button>',
+            },
+        },
+    },
 };
