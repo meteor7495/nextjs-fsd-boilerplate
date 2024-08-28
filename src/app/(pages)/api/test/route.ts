750503c5f1d4b81adcca349619b397a850143e4c
meteor7495 <job.mohiti@gmail.com>
2024-08-28 11:45:57 +0330

    .

--- src/app/(pages)/api/test/route.ts
+++ src/app/(pages)/api/test/route.ts
@@ -1,19 +1,19 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request): Promise<NextResponse> {
-  // Set the delay duration (in milliseconds)
-  const delayDuration = 2000; // 2 seconds delay
+    // Set the delay duration (in milliseconds)
+    const delayDuration = 2000; // 2 seconds delay
 
-  // Create a promise that resolves after the delay
-  const delayedResponse: Promise<NextResponse> = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(
-        NextResponse.json({
-          message: 'Click Me!',
-        }),
-      );
-    }, delayDuration);
-  });
-  // Wait for the delayed promise to resolve
-  return delayedResponse;
+    // Create a promise that resolves after the delay
+    const delayedResponse: Promise<NextResponse> = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(
+                NextResponse.json({
+                    message: 'Click Me!',
+                }),
+            );
+        }, delayDuration);
+    });
+    // Wait for the delayed promise to resolve
+    return delayedResponse;
 }
--- src/app/_app/providers/with-query.tsx
+++ src/app/_app/providers/with-query.tsx
@@ -5,14 +5,14 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 interface IProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export const ReactQueryProvider = ({ children }: IProps) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 };
