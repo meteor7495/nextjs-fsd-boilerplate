53b26073f64cb1d46e43d695f39b05a39220042a
meteor7495 <job.mohiti@gmail.com>
2024-09-23 15:54:09 +0330

    .

--- src/app/_shared/utils/configs/index.ts
+++ src/app/_shared/utils/configs/index.ts
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const OPENID_CLIENT_ID = 'jame_online_client_spa';
-export const OPENID_ISSUER = 'https://stageoauth.agah.com';
+export const OPENID_CLIENT_ID = '';
+export const OPENID_ISSUER = '';
 export const OPENID_CLIENT_SECRET = '';
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
